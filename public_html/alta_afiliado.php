45349a145e30802bd30ccb6dfcf16781c824a30c
Andres Norberto Schuster <andi.schus@live.com.ar>
2016-11-07 17:12:46 -0300

    Creacion de phps
    Creacion de phps, y modificacion de algunos nombres para que sea mas
    entendible

---
+++ public_html/alta_afiliado.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once 'templates/navbar.html';
+include_once 'templates/alta_afiliado.html';
+?>
+
---
+++ public_html/turnos.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'templates/navbar.html';
+include_once 'templates/turnos_disponibles.html';
+?>
