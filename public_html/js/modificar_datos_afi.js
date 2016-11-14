c3ca8838eea215a1853b88a7e09d7d0ad75c16e4
gusta2015 <gusti.daniel@yahoo.com.ar>
2016-11-14 19:43:25 -0300

    Modificacion de Afiliado
    Casi terminada la modificacion de Afiliado. Faltan Modales
    Correccion de Registro (cuando el afiliado recibe invitacion). Semi-funcionando

--- public_html/js/modificar_datos_afi.js
+++ public_html/js/modificar_datos_afi.js
@@ -6,11 +6,13 @@
 
 $(document).ready(function () {
     cargar_obras();
+    cargar_datos();
 
+//
     $("#form_afiliado").on('submit', function (event) {
         event.preventDefault();
 
-        if (validar()) {
+        if (validar() && validar_pass()) {
             var parametros = {
                 "nombre": $("#txtNombre").val(),
                 "apellido": $("#txtApellido").val(),
@@ -19,12 +21,14 @@ $(document).ready(function () {
                 "fecha_nacimiento": $("#dateNac").val(),
                 "mail": $("#txtMail").val(),
                 "os": $("#selObraSocial").val(),
-                "numAfi": $("#txtAfiliado").val(),
+                "numAfi": $("#numAfiliado").val(),
                 "localidad": $("#txtLocalidad").val(),
                 "direccion": $("#txtDireccion").val(),
                 "telefono": $("#numTelefono").val(),
                 "celular": $("#numCelular").val(),
-                "comentarios": $("#txtComentarios").val()
+                "id_usuario": 21,
+                "password": $("#txtPass").val()
+//                "comentarios": $("#txtComentarios").val()
             };
             $.ajax({
                 data: parametros,
@@ -41,7 +45,7 @@ $(document).ready(function () {
                 }
             });
 
-        } else{
+        } else {
             console.log("Formulario No validado");
         }
 
@@ -53,17 +57,47 @@ $(document).ready(function () {
 
 
 
+function cargar_datos() { //Carga datos que actualmente estan guardados del cliente
+
+    $.ajax({
+        data: {"id_afiliado": 21},
+        url: 'conexiones/datos_afi.php',
+        method: "GET",
+        success: function (response) {
+            var datos = JSON.parse(response); // la respuesta del servidor es un JSON
+
+            $("#txtNombre").val(datos.nombre);
+            $("#txtApellido").val(datos.apellido);
+            $("#numDNI").val(datos.dni);
+            $("#selGenero").val(datos.genero),
+                    $("#dateNac").val(datos.fecha_nacimiento);
+            $("#txtMail").val(datos.mail);
+            $("#selObraSocial").val(datos.id_obra_social),
+                    $("#numAfiliado").val(datos.numero_afiliado);
+            $("#txtLocalidad").val(datos.localidad);
+            $("#txtDireccion").val(datos.direccion);
+            $("#numTelefono").val(datos.telefono);
+            $("#numCelular").val(datos.celular);
+//            $("#txtComentarios").val()
+
+
+//            $('#selObraSocial').append("<option value='default' selected='' style='color: red'>SELECCIONA OBRA SOCIAL</option>");
+
+        }
+    });
+}
 function cargar_obras() { //cargar Obras Sociales en el select correspondiente
     $.ajax({
         url: 'conexiones/obras_sociales.php',
         success: function (response) {
             //console.log(response);
             $('#selObraSocial').html(response);
-            $('#selObraSocial').append("<option value='default' selected='' style='color: red'>SELECCIONA OBRA SOCIAL</option>");
+            // $('#selObraSocial').append("<option value='default' selected='' style='color: red'>SELECCIONA OBRA SOCIAL</option>");
 
         }
     });
 }
+
 
 function validar() {
     if ($("#selObraSocial").val() === "default") {
@@ -73,4 +107,13 @@ function validar() {
     } else {
         return true;
     }
+}
+
+function validar_pass() {
+    if ($("#txtPass") === $("#txtPass2")) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
--- public_html/js/registro.js
+++ public_html/js/registro.js
@@ -1,10 +1,78 @@
-function enviarDatos(){
-    $("#divPassword").show();
-    $("#divPassword2").show();
-    $("#btnEnviar").val("Registrarme");
-};
-
-$(document).ready(function(){
+$(document).ready(function () {
     $("#divPassword").hide();
     $("#divPassword2").hide();
+    $("#divCompletar").hide();
+
+    $("#formRegistro").on("submit", function(event){
+        event.preventDefault();
+        var usuario_encontrado=enviarDatos();
+        
+        
+        if (usuario_encontrado){
+                $("#divPassword").show();
+                $("#divPassword2").show();                
+                $("#divCompletar").show();
+
+                $("#divComprobar").hide();
+                
+                $("#btnRegistrar").click(function(){
+                    console.log(usuario_encontrado);
+                    
+                });
+        }       
+    });
 });
+
+
+function enviarDatos() {
+    
+    var parametros = {
+        "dni": $("#numDNI").val(),
+        "mail": $("#txtMail").val()
+    };
+
+    $.ajax({
+        data: parametros,
+        url: 'conexiones/registro_final.php',
+        method: "POST",
+        success: function (response) {
+            if (!isNaN(response)) {
+                var id_usuario = response;
+                return id_usuario;
+            } else {
+                $('#divErrores').modal();                      // initialized with defaults
+                $('#divErrores').modal({ keyboard: false });   // initialized with no keyboard
+                $('#divErrores').modal('show');     
+                return false;
+            }return id_usuario;
+        }
+        
+    }); 
+}
+
+function completarRegistro(id){
+    var parametros={
+        "pass":$("#txtPass").val(),
+        "id_usuario":id
+    };
+        $.ajax({
+        data: parametros,
+        url: 'conexiones/registro_completo.php',
+        method: "POST",
+        success: function (response) {
+            if (response) {
+//                console.log(response);
+            }
+        }
+    });
+    
+}
+
+function validar_pass() {
+    if ($("#txtPass") === $("#txtPass2")) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
