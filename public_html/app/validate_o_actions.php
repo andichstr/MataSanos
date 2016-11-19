1fdbc00fe4f60c093e048d6dd0cd955de56920b1
Diego Rodriguez <diego.ba.rodriguez@gmail.com>
2016-11-18 21:58:19 -0300

    Validar acciones de operador y afiliado
    Valida que solo pueda acceder un operador y un afiliado a una
    funcionalidad compartida.
    En ambos casos devuelve el id del afiliado.
    Devuelve falso si no se cumplen las condiciones.

--- public_html/app/validate_o_actions.php
+++ public_html/app/validate_o_actions.php
@@ -2,11 +2,17 @@
 //Este modulo valida que el que accede a una funcionalidad de un afiliado es un operador
 //la funcion validar_o() devuelve False si no esta la variable que guarda el id del afiliado ó el que accede no es un operador. 
 // de lo contrario devuelte el id del afiliado para usarlo en donde corresponda.
+// Si el que ingresa es un afiliado devuelve el id del mismo.
 
 function validar_o(){
 	if (isset($_SESSION['id_afiliado']) && ($_SESSION['roleuser'] == 2) {
 		//me guardo el id_afiliado
 		$id_afi = $_SESSION['id_afiliado'];
 		return $id_afi;
+	elseif (isset($_SESSION['userid'])) && ($_SESSION['roleuser'] == 1) {
+		//me guardo el id_afiliado
+		$id_afi = $_SESSION['userid'];
+		return $id_afi;	
+	}
 	}else{return False;}
 }
