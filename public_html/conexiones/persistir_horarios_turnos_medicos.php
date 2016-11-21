2f7ff0c2833dcd38edc3905aa596c9fe90ac54f1
gusta2015 <gusti.daniel@yahoo.com.ar>
2016-11-21 02:33:54 -0300

    Persistir turnos incompleto
    no funciona. Falta la logica para setear los datos correctos a partir de los horarios cargados

--- public_html/conexiones/persistir_horarios_turnos_medicos.php
+++ public_html/conexiones/persistir_horarios_turnos_medicos.php
@@ -3,13 +3,12 @@
 include_once 'configure.php';
 include_once 'conexion.php';
 
-print_r($_POST['especialidades']) ;
-print_r($_POST['horarios'][0]['duracion']) ;
-
+print_r($_POST['especialidades']);
+print_r($_POST['horarios'][0]['duracion']);
 
 function persistirTurnosMedicosHorarios() {//retornara los id de los turnos nuevos
     if (isset($_POST['id_medico']) && isset($_POST['especialidades']) && isset($_POST['horarios'])) {
-        $turnos_persistir=[]; //array que guardara los id de los turnos que se generaros en la ultima alta.
+        $turnos_persistir = []; //array que guardara los id de los turnos que se generaros en la ultima alta.
         $id_medico = $_POST['id_medico'];
         $especialidades = $_POST['especialidades'];
         $horarios = $_POST['horarios'];
@@ -17,15 +16,15 @@ function persistirTurnosMedicosHorarios() {//retornara los id de los turnos nuev
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $query = $con->prepare("INSERT INTO " . tabla_horarios_turnos_medicos . " (id_medico, id_especialidad, dia, horario_inicio, horario_fin, duracion_turno_minutos) VALUES (:id_medico, :id_especialidad, :dia, :horario_inicio, :horario_fin, :duracion)");
         $query->bindParam(':id_medico', $id_medico);
-        for ($i=0; $i<count($especialidades); $i++){
+        for ($i = 0; $i < count($especialidades); $i++) {
             $query->bindParam(':id_especialidad', $especialidades[$i]);
-            for ($a=0; $a<count($horarios); $a++){
-                $query->bindParam(':dia',$horarios[$a]['dia']);
-                $query->bindParam(':horario_inicio',$horarios[$a]['horarioInicio']);
-                $query->bindParam(':horario_fin',$horarios[$a]['horarioFin']);
-                $query->bindParam(':duracion',$horarios[$a]['duracion']);
+            for ($a = 0; $a < count($horarios); $a++) {
+                $query->bindParam(':dia', $horarios[$a]['dia']);
+                $query->bindParam(':horario_inicio', $horarios[$a]['horarioInicio']);
+                $query->bindParam(':horario_fin', $horarios[$a]['horarioFin']);
+                $query->bindParam(':duracion', $horarios[$a]['duracion']);
                 if ($query->execute()) {
-                    $turnos_persistir[]=$con->lastInsertId();
+                    $turnos_persistir[] = $con->lastInsertId();
                     $resp = 'Sisi';
                 } else {
                     $resp = 'Nono';
@@ -36,6 +35,31 @@ function persistirTurnosMedicosHorarios() {//retornara los id de los turnos nuev
         return $turnos_persistir;
     } else {
         echo 'Campos no seteados';
+    }
+    $con = NULL;
+}
+
+function persistir_turnos($turnos) {
+    $con = new Conexion();
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = $con->prepare("SELECT * " . tabla_horarios_turnos_medicos . " WHERE id_horarios_turnos_medicos=:id");
+//        $query->bindParam(':id_medico', $id_medico);
+    for ($a = 0; $a < count($turnos); $a++) { //por cada turno nuevo para persistir..
+        $query->bindParam(':dia', $turnos[$a]);
+        if ($query->execute()) {
+            $turno = $query->fetchAll();
+            
+            
+            $con2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//insertar turno..
+            $query2 = $con->prepare("INSERT INTO " . tabla_turnos . "(horario, fecha, id_medico, id_especialidad) VALUES (:horario, :fecha, :medico, :especialidad)");
+            
+            
+            
+            
+            
+        } else {
+            $resp = 'Nono';
+        }
     }
 }
 
