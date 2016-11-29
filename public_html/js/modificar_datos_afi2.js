932f9b4bf75ce4486e0c051e57b9977ec59a8ece
Andres Norberto Schuster <andi.schus@live.com.ar>
2016-11-29 15:36:35 -0300

    Avances con mensajes de validaciones

--- public_html/js/modificar_datos_afi2.js
+++ public_html/js/modificar_datos_afi2.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	$('h1').text('Modificar datos de afiliado');
-	form();
+    $('h1').text('Modificar datos de afiliado');
+    form();
     cargar_obras();
     cargar_datos();
 
@@ -9,8 +9,8 @@ $(document).ready(function () {
 
         if (validar()) {
             var parametros = {
-				"numAfi": $("#numAfiliado").val(),
-				"dni": $("#numDNI").val(),
+                "numAfi": $("#numAfiliado").val(),
+                "dni": $("#numDNI").val(),
                 "nombre": $("#txtNombre").val(),
                 "apellido": $("#txtApellido").val(),
                 "genero": $("#selGenero").val(),
@@ -21,75 +21,74 @@ $(document).ready(function () {
                 "direccion": $("#txtDireccion").val(),
                 "telefono": $("#numTelefono").val(),
                 "celular": $("#numCelular").val(),
-                "comentarios": $("#txtComentarios").val(),
+                "comentarios": $("#txtComentarios").val()
             };
             $.ajax({
                 data: parametros,
                 url: this.action,
                 type: this.method,
                 success: function (response) {
-					console.log(response);
-                    if (response==true) {
-                        $('p#pmsj1').text('Los datos del Afiliado '+$("#txtNombre").val()+' '+$("#txtApellido").val());
-						$('p#pmsj2').text('Han sido modificados correctamente');
-						$('#divInforme').modal()       
-						$('#divInforme').modal({ keyboard: false })
-						$('#divInforme').modal('show') 
+                    console.log(response);
+                    if (response == true) {
+                        $('p#pmsj1').text('Los datos del Afiliado ' + $("#txtNombre").val() + ' ' + $("#txtApellido").val());
+                        $('p#pmsj2').text('Han sido modificados correctamente');
+                        $('#divInforme').modal();
+                        $('#divInforme').modal({keyboard: false});
+                        $('#divInforme').modal('show');
                     } else {
                         console.log(response);
                         $('p#pmsj1').text(response);
-						$('p#pmsj2').text('');
-						$('#divInforme').modal()       
-						$('#divInforme').modal({ keyboard: false })
-						$('#divInforme').modal('show')    
+                        $('p#pmsj2').text('');
+                        $('#divInforme').modal();
+                        $('#divInforme').modal({keyboard: false});
+                        $('#divInforme').modal('show');
                     }
                 }
             });
 
         } else {
             $('p#pmsj1').text('Datos no validos. Revisa el formulario');
-			$('p#pmsj2').text('');
-			$('#divInforme').modal()       
-			$('#divInforme').modal({ keyboard: false })
-			$('#divInforme').modal('show')
-        }
-
-
+            $('p#pmsj2').text('');
+            $('#divInforme').modal();
+            $('#divInforme').modal({keyboard: false});
+            $('#divInforme').modal('show');
+        };
     });
-
-
 });
 
-
-
 function cargar_datos() { //Carga datos que actualmente estan guardados del cliente
-
     $.ajax({
         url: 'conexiones/datos_afi.php',
         method: "POST",
         success: function (response) {
             var datos = JSON.parse(response); // la respuesta del servidor es un JSON
-			console.log(datos);
+            console.log(datos);
             $("#txtNombre").val(datos[0].nombre);
             $("#txtApellido").val(datos[0].apellido);
             $("#numDNI").val(datos[0].dni);
             $("#selGenero").val(datos[0].genero);
             $("#dateNac").val(datos[0].fecha_nacimiento);
             $("#txtMail").val(datos[0].mail);
-            $("#selObraSocial").val(datos[0].id_obra_social),
+            $("#selObraSocial").val(datos[0].id_obra_social);
             $("#numAfiliado").val(datos[0].numero_afiliado);
             $("#txtLocalidad").val(datos[0].localidad);
             $("#txtDireccion").val(datos[0].direccion);
             $("#numTelefono").val(datos[0].telefono);
             $("#numCelular").val(datos[0].celular);
             $("#txtComentarios").text(datos[0].comentarios);
-            $("p.nota strong").text('Modificando datos de: '+datos[0].nombre+' '+datos[0].apellido);
-            if (datos[1]==false){
-				$("input#txtMail").attr('disabled',true);
-			}
+            $("p.nota strong").text('Modificando datos de: ' + datos[0].nombre + ' ' + datos[0].apellido);
+            if (datos[1] == false) {
+                $("input#txtMail").attr('disabled', true);
+            }
+            $("#form_afiliado :input").each(function () {
+                $(this).keyup(function (event) {
+                    validar();
+                });
+            });
         }
-    });  
+    });
 }
+
 function cargar_obras() { //cargar Obras Sociales en el select correspondiente
     $.ajax({
         url: 'conexiones/obras_sociales.php',
@@ -99,32 +98,32 @@ function cargar_obras() { //cargar Obras Sociales en el select correspondiente
         }
     });
 }
-function form(){
-	$("div.pass").hide();
-	$("input.ope").removeAttr('disabled');
-	$("select.ope").removeAttr('disabled');
-	$("div#info").removeAttr('hidden');
+function form() {
+    $("div.pass").hide();
+    $("input.ope").removeAttr('disabled');
+    $("select.ope").removeAttr('disabled');
+    $("div#info").removeAttr('hidden');
 }
 
 function validar() {
     jQuery.validator.setDefaults({
-	debug: true,
-	success: "valid"
-	});
-	var validator = $("#form_afiliado").validate({
+        debug: true,
+        success: "valid"
+    });
+    var validator = $("#form_afiliado").validate({
         rules: {
-            numAfiliado: { required: true, minlength: 1 },
-            numDNI: { required: true, minlength: 8, number: true },
-            txtNombre: { required: true, lettersonly: true, minlength: 3 },
-            txtApellido: { required: true, lettersonly: true, minlength: 3 },
-            selGenero: { required:true },
-            dateNac: { required:true },
-            txtMail: { required: true, email: true },
-            selObraSocial: { required: true },
-            txtLocalidad: { required: true },
-            txtDireccion: { required: true },
-            numTelefono: { number: true, minlength: 8},
-            numCelular: { number: true, minlength: 10 },
+            numAfiliado: {required: true, minlength: 1},
+            numDNI: {required: true, minlength: 6, maxlength: 10, number: true},
+            txtNombre: {required: true, lettersonly: true, minlength: 3},
+            txtApellido: {required: true, lettersonly: true, minlength: 3},
+            selGenero: {required: true},
+            dateNac: {required: true},
+            txtMail: {required: true, email: true},
+            selObraSocial: {required: true},
+            txtLocalidad: {required: true},
+            txtDireccion: {required: true},
+            numTelefono: {number: true, minlength: 8},
+            numCelular: {number: true, minlength: 10},
         },
         messages: {
             numAfiliado: "Ingresa un dato valido.",
@@ -139,9 +138,16 @@ function validar() {
             txtDireccion: "Ingresa una direccion válida",
             numTelefono: "Ingresa un telefono válido",
             numCelular: "No puedes modificar este campo",
-            
         },
-     });
-     return validator.form();
+        errorPlacement: function (error, element) {
+            element.attr('oninvalid', 'setCustomValidity(' + error.html() + ');');
+            if (error.html() != "") {
+                element.attr('style', 'color: #FF0000');
+            } else {
+                element.attr('style', 'color: #000000');
+            }
+        }
+    });
+    return validator.form();
 }
 
